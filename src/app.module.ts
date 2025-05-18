570930f9fea7a3069c401108e93b6637a332dee5
lremi <remileroi.pro@gmail.com>
2025-05-18 22:19:55 +0200

    feat: Add environment configuration and update Docker setup for improved security and flexibility

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -8,7 +9,6 @@ import { MealsModule } from './meals/meals.module';
 import { FoodItemsModule } from './food-items/food-items.module';
 import { DaySummaryModule } from './day-summary/day-summary.module';
 import { FoodItem } from './food-items/food-item.entity';
-import { ConfigModule } from '@nestjs/config';
 import { NutritionModule } from './nutrition/nutrition.module';
 import { FoodItemsSeeder } from './database/food-items.seed';
 
@@ -16,19 +16,25 @@ import { FoodItemsSeeder } from './database/food-items.seed';
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
+      envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'db',
-      port: 5432,
-      username: 'admin',
-      password: 'admin',
-      database: 'foodlog',
-      synchronize: true,
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get<string>('DB_HOST'),
+        port: configService.get<number>('DB_PORT'),
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        synchronize: configService.get<string>('NODE_ENV') !== 'production',
+        logging: configService.get<string>('NODE_ENV') === 'development',
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      }),
+      inject: [ConfigService],
     }),
     TypeOrmModule.forFeature([FoodItem]),
-    AuthModule, // Ajout du module d'authentification
+    AuthModule,
     UsersModule,
     MealsModule,
     FoodItemsModule,
